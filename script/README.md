3ad7a7b821d6c3caa13efee408e2d1ed55a89877
David Fairbrother <DavidFair@users.noreply.github.com>
2024-02-13 15:44:20 +0000

    ENH: Add script to upload all images
    Since we now have a number of images to manage, add a Python script to
    automatically upload these with the appropriate names...etc. using the
    OpenStack SDK
    
    Longer-term it would be nice to drive the builds with Python but small
    incremental steps first

--- script/README.md
+++ script/README.md
@@ -1,3 +1,25 @@
 Helper scripts for building multiple images
 
-- capi-all.sh  - Build all CAPI images based on the JSON files in the `versions` directory
+- build-all.sh  - Build all CAPI images based on the JSON files in the `versions` directory
+- upload_all.py - Upload all CAPI images to OpenStack based on the os-cloud provided
+
+Upload All
+==========
+
+This automatically globs all the output files in the k8s-image-builder directory and uploads them to OpenStack with the
+name format including today's date.
+
+Preparation
+-----------
+
+- Create a venv
+- Install the requirements from `requirements.txt`
+- Build your images with `build-all.sh` or manually following the readme in the `cluster-api` directory
+
+Usage
+-----
+
+`upload_all.py` requires the name of the OS cloud to upload to. It also has the following optional arguments:
+- `--dry-run` - Do not upload the images, just print the commands
+- `--public` - If specified the images will be public, otherwise they will be private to the project
+
---
+++ script/test/test_upload_all.py
@@ -0,0 +1,166 @@
+from datetime import datetime
+from pathlib import Path
+from typing import Dict
+from unittest import mock
+from unittest.mock import Mock, call
+
+import pytest
+
+from upload_all import glob_output_files, Args, parse_args, upload_images_to_openstack, upload_single_image, main, \
+    get_upload_name
+
+
+def test_glob_output_files(tmp_path):
+    """
+    Test that the glob_output_files function returns the expected files
+    and does not include directories
+    :param tmp_path: A temp path setup by pytest
+    """
+    expected_output_path = tmp_path / "k8s-image-builder" / "images" / "capi" / "output"
+    expected_output_path.mkdir(parents=True)
+
+    expected = ["ubuntu-kube-1.23.4", "ubuntu-kube-1.40.0"]
+    for out_name in expected:
+        # Make parent dir as this is a named dir with a single file
+        (expected_output_path / out_name).mkdir(parents=True)
+        (expected_output_path / out_name / out_name).touch()
+        # And a fake file in each
+        (expected_output_path / out_name / "should_not_appear").touch()
+
+    with mock.patch("git.Repo") as mocked_repo:
+        mocked_repo.return_value.working_tree_dir = str(tmp_path)
+        returned = list(glob_output_files())  # Avoid exhausting the generator
+
+    expected = sorted(str(expected_output_path / file / file) for file in expected)
+    assert sorted(str(file) for file in returned) == expected
+    assert len(returned) == len(expected)
+
+
+def test_arg_parse_all_true():
+    """
+    Tests that the parse_args function returns the expected Args object
+    if the user sets all optional arguments
+    """
+    user_input = ["--dry-run", "--public", "default"]
+    expected = Args(dry_run=True, public=True, os_cloud="default")
+    assert parse_args(user_input) == expected
+
+
+def test_arg_parse_no_optional_args():
+    """
+    Tests that the parse_args function returns the expected Args object
+    if the user does not set any optional arguments
+    """
+    user_input = ["bar"]
+    expected = Args(dry_run=False, public=False, os_cloud="bar")
+    assert parse_args(user_input) == expected
+
+
+def test_arg_parse_no_args():
+    """
+    Tests that the parse_args function exits with a SystemExit
+    if the user does not set the OS cloud argument
+    """
+    user_input = []
+    with pytest.raises(SystemExit):
+        parse_args(user_input)
+
+
+def test_upload_images_to_openstack_dry_run():
+    """
+    Tests that the upload_images_to_openstack function does not call
+    anything uploading to OpenStack if the dry_run flag is set
+    """
+    expected_files = [Path("file1"), Path("file2")]
+    args = Args(dry_run=True, public=True, os_cloud="default")
+
+    with mock.patch("upload_all.upload_single_image") as mocked_upload_single_image:
+        upload_images_to_openstack(expected_files, args)
+    assert mocked_upload_single_image.call_count == 0
+
+
+def _expected_args_helper(file: Path, visibility: str) -> Dict:
+    """
+    Helper function to generate the expected arguments for upload_single_image
+    :param file: The Path of a file to upload
+    :param visibility: A string of either "public" or "private"
+    :return: A dictionary of expected arguments for an assert call list
+    """
+    return {
+        "name": get_upload_name(file),
+        "filename": file.as_posix(),
+        "disk_format": "qcow2",
+        "container_format": "bare",
+        "wait": True,
+        "visibility": visibility}
+
+
+def test_upload_images_to_openstack_real_run():
+    """
+    Tests that the upload_images_to_openstack function calls
+    upload_single_image with the expected arguments
+    """
+    expected_files = [Path("file1"), Path("file2")]
+
+    args = Args(dry_run=False, public=True, os_cloud="default")
+    with mock.patch("upload_all.upload_single_image") as mocked_upload_single_image:
+        upload_images_to_openstack(expected_files, args)
+
+    assert mocked_upload_single_image.call_count == 2
+    expected = [call(
+        args.os_cloud,
+        _expected_args_helper(file, "public")) for file in expected_files]
+    assert mocked_upload_single_image.call_args_list == expected
+
+
+def test_upload_images_with_private_annotation():
+    """
+    Tests that the upload_images_to_openstack function calls
+    upload_single_image with the expected arguments if the public flag is not set
+    """
+    args = Args(dry_run=False, public=False, os_cloud="foo")
+    with mock.patch("upload_all.upload_single_image") as mocked_upload_single_image:
+        upload_images_to_openstack([Path("file1")], args)
+
+    assert mocked_upload_single_image.call_count == 1
+    expected = [call(
+        args.os_cloud,
+        _expected_args_helper(Path("file1"), "private"))]
+    assert mocked_upload_single_image.call_args_list == expected
+
+
+def test_upload_single_image():
+    """
+    Tests the connection and image upload forwards the args as-is
+    """
+    fake_args = {"foo": "bar", "baz": "qux"}
+    with mock.patch("openstack.connect") as mocked_openstack_connect:
+        mocked_openstack_connect.return_value.create_image = Mock()
+        upload_single_image("fake_cloud", fake_args)
+
+    mocked_openstack_connect.assert_called_once_with(cloud="fake_cloud")
+    mocked_openstack_connect.return_value.create_image.assert_called_once_with(**fake_args)
+
+
+def test_get_upload_name():
+    """
+    Tests that the name generator returns the expected name
+    with the format capi-{file.name}-{today's date}
+    """
+    input_name = "ubuntu-kube-1.23.4"
+    expected = f"capi-{input_name}-{datetime.today().strftime('%Y-%m-%d')}"
+    print(f"Expected: {expected}")
+    assert get_upload_name(Path(input_name)) == expected
+
+
+@mock.patch("upload_all.glob_output_files")
+@mock.patch("upload_all.upload_images_to_openstack")
+def test_main_steps(mock_upload, mock_glob):
+    """
+    Tests the main flow ordering of the script
+    """
+    args = Args(dry_run=True, public=True, os_cloud="default")
+    main(args)
+
+    mock_glob.assert_called_once()
+    mock_upload.assert_called_once_with(mock_glob.return_value, args)
