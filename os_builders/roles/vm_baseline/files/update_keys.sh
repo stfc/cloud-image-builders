49066757f4c45310c62b56ab6bc16b3ec5b350bc
Alexander Dibbo <alexander.dibbo@stfc.ac.uk>
2024-08-08 10:58:40 +0100

    Dont sent emails for now

--- os_builders/roles/vm_baseline/files/update_keys.sh
+++ os_builders/roles/vm_baseline/files/update_keys.sh
@@ -9,7 +9,7 @@ fi
 
 if lsattr $KEYSPATH/.ssh/authorized_keys | grep "\-i\-"
 then
-    echo "file $KEYSPATH/.ssh/authorized_keys is immutable, cannot write admin key list to it. Generated from \`update_keys.sh\`." | mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk
+    echo "file $KEYSPATH/.ssh/authorized_keys is immutable, cannot write admin key list to it. Generated from \`update_keys.sh\`." #| mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk
 else
     wget http://openstack.nubes.rl.ac.uk:9999/admin_key_list
     if [ -f admin_key_list ]
@@ -17,11 +17,11 @@ else
         mv admin_key_list /root/.ssh/authorized_keys
         if [ ! -s $KEYSPATH/.ssh/authorized_keys ]
         then
-            echo "file $KEYSPATH/.ssh/admin_key_list is empty. The download from service node failed. Generated from \`update_keys.sh\`." | mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk
+            echo "file $KEYSPATH/.ssh/admin_key_list is empty. The download from service node failed. Generated from \`update_keys.sh\`." #| mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk
         else
-            grep "Alexander Dibbo" $KEYSPATH/.ssh/authorized_keys || { echo "file $KEYSPATH/.ssh/admin_key_list does not contain the correct keys. Generated from \`update_keys.sh\`." | mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk ; }
+            grep "Alexander Dibbo" $KEYSPATH/.ssh/authorized_keys || { echo "file $KEYSPATH/.ssh/admin_key_list does not contain the correct keys. Generated from \`update_keys.sh\`." #| mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk ; }
         fi
     else
-        echo "file $KEYSPATH/.ssh/admin_key_list does not exist. The download from service node failed. Generated from \`update_keys.sh\`." | mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk
+        echo "file $KEYSPATH/.ssh/admin_key_list does not exist. The download from service node failed. Generated from \`update_keys.sh\`." #| mail -s "Error during VM authorized_keys update" cloud-support@stfc.ac.uk
     fi
 fi
