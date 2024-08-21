79bec5e36bdd34bd68736cce506e06d174126750
Alexander Dibbo <alexander.dibbo@stfc.ac.uk>
2024-08-21 11:41:34 +0100

    Fix issues with user management script

--- os_builders/roles/nubes_bootcontext/files/update_cloud_users.sh
+++ os_builders/roles/nubes_bootcontext/files/update_cloud_users.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-for i in packer packer-test;
-do
-    userdel $i -r;
-done
+if [[ -f /var/lock/firstboot ]] ;
+then
+    for i in packer packer-test;
+    do
+        id -u $i && userdel $i -r;
+    done
+    rm -f /var/lock/firstboot
+fi
 
-mkdir /mnt/context
-mount /dev/sr0 /mnt/context
+[[ -d /mnt/context ]] || mkdir /mnt/context
+[[ -d /mnt/context/openstack ]] || mount /dev/sr0 /mnt/context
 INSTANCEID=$(jq .uuid /mnt/context/openstack/latest/meta_data.json | sed "s/\"//g")
 
-if curl -s http://openstack.nubes.rl.ac.uk:9999/cgi-bin/get_username_list.sh?"$INSTANCEID" | grep ".";
+if curl -s http://openstack.nubes.rl.ac.uk:9999/cgi-bin/get_username.sh?"$INSTANCEID" | grep ".";
 then
     OPENSTACK_URL='openstack.nubes.rl.ac.uk'
 else
@@ -35,26 +39,25 @@ SSH_PUBLIC_KEY=$(jq .keys[0].data /mnt/context/openstack/latest/meta_data.json |
 
 groupadd wheel
 
-for ID in $FEDIDS; do
-    useradd "$ID" -g wheel -m -s /bin/bash
+for ID in $FEDID $FEDIDS; do
+    id -u $ID || useradd "$ID" -g wheel -m -s /bin/bash
     usermod "$ID" -a -G wheel
-    SUDOVALID=$(visudo -c -f /etc/sudoers.d/cloud)
-    if [ "$SUDOVALID" != "/etc/sudoers.d/cloud: parsed OK" ]
+    if visudo -c -f /etc/sudoers.d/cloud;
     then
         echo " $ID  ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/cloud
     fi
-    if grep -q "$ID" /etc/sudoers.d/cloud
+    if ! grep -q "$ID" /etc/sudoers.d/cloud
     then
-        true
-    else
+        #true
+    #else
         echo " $ID  ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers.d/cloud
     fi
     chmod 0440 /etc/sudoers.d/cloud
-    mkdir -p /home/"$ID"/.ssh
+    [[ -d /home/"$ID"/.ssh ]] || mkdir -p /home/"$ID"/.ssh
     chown "$ID" /home/"$ID"
     chown "$ID" /home/"$ID"/.ssh
     if [[ "$ID" == "$FEDID" ]]; then
         echo "$SSH_PUBLIC_KEY "| sed 's/\\n//g' >> /home/"$ID"/.ssh/authorized_keys
     fi
-    chown "$ID" /home/"$FEDID"/.ssh/authorized_keys
+    chown "$ID" /home/"$ID"/.ssh/authorized_keys
 done
