b6266c161499ca954e750e83b45ddb38d52cec84
Alexander Dibbo <alexander.dibbo@stfc.ac.uk>
2024-09-23 10:10:54 +0100

    Address black issues

--- os_builders/roles/vm_baseline/files/wazuh/enrich-wazuh-agent.py
+++ os_builders/roles/vm_baseline/files/wazuh/enrich-wazuh-agent.py
@@ -8,9 +8,11 @@ import xml.etree.ElementTree
 import shutil
 import pathlib
 
+
 source = "/var/ossec/etc/ossec.template"
 destination = "/var/ossec/etc/ossec.conf"
 extra_config_dir = '/var/ossec/etc/extra/'
+
 
 def dict_to_xml(tag, d):
 
@@ -23,6 +25,7 @@ def dict_to_xml(tag, d):
         elem.append(child)
 
     return elem
+
 
 # Open template config file
 with open(source) as sourcefile:
@@ -56,83 +59,96 @@ ossec_conf = xml.etree.ElementTree.parse(source)
 ossec_xml = ossec_conf.getroot()
 
 
-LABELS_STRING=""
-AGENT_HOSTNAME=socket.getfqdn()
+LABELS_STRING = ""
+AGENT_HOSTNAME = socket.getfqdn()
 
 # Check if the host is an OpenStack VM
 try:
     metadata_url = "http://169.254.169.254/openstack/latest/meta_data.json"
     response = requests.get(metadata_url)
     openstack_metadata = response.json()
-    LABELS_STRING = "{0}<label key=\"{1}\">{2}</label>".format(LABELS_STRING, "openstack.uuid", openstack_metadata["uuid"])
-    LABELS_STRING = "{0}<label key=\"{1}\">{2}</label>".format(LABELS_STRING, "openstack.name", openstack_metadata["name"])
-    LABELS_STRING = "{0}<label key=\"{1}\">{2}</label>".format(LABELS_STRING, "openstack.hostname", openstack_metadata["hostname"])
-    LABELS_STRING = "{0}<label key=\"{1}\">{2}</label>".format(LABELS_STRING, "openstack.project_id", openstack_metadata["project_id"])
+    LABELS_STRING = '{0}<label key="{1}">{2}</label>'.format(
+        LABELS_STRING, "openstack.uuid", openstack_metadata["uuid"]
+    )
+    LABELS_STRING = '{0}<label key="{1}">{2}</label>'.format(
+        LABELS_STRING, "openstack.name", openstack_metadata["name"]
+    )
+    LABELS_STRING = '{0}<label key="{1}">{2}</label>'.format(
+        LABELS_STRING, "openstack.hostname", openstack_metadata["hostname"]
+    )
+    LABELS_STRING = '{0}<label key="{1}">{2}</label>'.format(
+        LABELS_STRING, "openstack.project_id", openstack_metadata["project_id"]
+    )
     AGENT_HOSTNAME = AGENT_HOSTNAME + "-" + openstack_metadata["uuid"]
 except:
     print("not an openstack VM")
 
 
-
 if os.path.exists("/etc/ccm.conf"):
     print("Is Quattor Managed")
     # Get aquilon personality from profile
-    p = subprocess.Popen(["/usr/sbin/ccm", "/system/personality/name"], bufsize=1, stdout=subprocess.PIPE)
+    p = subprocess.Popen(
+        ["/usr/sbin/ccm", "/system/personality/name"], bufsize=1, stdout=subprocess.PIPE
+    )
     personality = p.communicate()
     # Remove cruft from string
-    personality = str(personality[0]).replace('b"$ name : ', "").replace("'\\n\\n\"", '"').replace("'", '"')
+    personality = (
+        str(personality[0])
+        .replace('b"$ name : ', "")
+        .replace("'\\n\\n\"", '"')
+        .replace("'", '"')
+    )
     # Format as XML and merge into Labels XML - probably a better way of doing this but hayho
-    labels_conf["aq.personality"] =  personality
+    labels_conf["aq.personality"] = personality
 
 
 # Commands for use with Active Responses
 for command_item in commands_conf:
-     new_command = dict_to_xml('command', command_item)
-     ossec_xml.append(new_command)
+    new_command = dict_to_xml("command", command_item)
+    ossec_xml.append(new_command)
 
 # Log Files
 for logfile_item in logfiles_conf:
-    new_logfile = dict_to_xml('localfile', logfile_item)
+    new_logfile = dict_to_xml("localfile", logfile_item)
     ossec_xml.append(new_logfile)
 
 # Security Configuration Assessments
 for sca_item in sca_conf:
-    new_sca = dict_to_xml('sca', sca_item)
+    new_sca = dict_to_xml("sca", sca_item)
     ossec_xml.append(new_sca)
 
 # Active Response
 for activeresponse_item in active_responses_conf:
-    new_activeresponse = dict_to_xml('active-response', activeresponse_item)
+    new_activeresponse = dict_to_xml("active-response", activeresponse_item)
     ossec_xml.append(new_activeresponse)
 
 # File Integrity monitoring
 for fim_item in fim_conf:
-    new_fim = dict_to_xml('syscheck', fim_item)
+    new_fim = dict_to_xml("syscheck", fim_item)
     ossec_xml.append(new_fim)
 
 # Labels
 for key, value in labels_conf.items():
-    new_label = xml.etree.ElementTree.SubElement( ossec_xml.find("labels"), 'label')
+    new_label = xml.etree.ElementTree.SubElement(ossec_xml.find("labels"), "label")
     new_label.text = value
-    new_label.attrib['key'] = key
+    new_label.attrib["key"] = key
 #labels_xml = ossec_xml.find('labels')
 
 # Wodles
 for wodle_key, wodle_item in wodles_conf.items():
-    new_wodle = dict_to_xml('wodle', wodle_item)
+    new_wodle = dict_to_xml("wodle", wodle_item)
     new_wodle.set("name", wodle_key)
     ossec_xml.append(new_wodle)
 
 # Hostname and Groups setup
 client_update = ossec_xml.find("client").find("enrollment")
-groups_update = xml.etree.ElementTree.SubElement(client_update, 'enrollment')
+groups_update = xml.etree.ElementTree.SubElement(client_update, "enrollment")
 group_seperator = ","
 groups_update.tag = "groups"
 groups_update.text = group_seperator.join(groups_conf)
-hostname_update = xml.etree.ElementTree.SubElement(client_update, 'enrollment')
+hostname_update = xml.etree.ElementTree.SubElement(client_update, "enrollment")
 hostname_update.tag = "agent_name"
 hostname_update.text = AGENT_HOSTNAME
-
 
 
 ossec_conf.write(destination)
