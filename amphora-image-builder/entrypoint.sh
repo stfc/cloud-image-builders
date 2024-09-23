045e9e544bafe99143f0e3afa975e5ac8b8d2665
Alexander Dibbo <alexander.dibbo@stfc.ac.uk>
2024-09-23 12:05:12 +0100

    Run local elements

--- amphora-image-builder/entrypoint.sh
+++ amphora-image-builder/entrypoint.sh
@@ -11,6 +11,13 @@ git clone --depth=1 https://github.com/openstack/octavia
 DIB_REPO_PATH="$(pwd)/octavia"
 export DIB_REPO_PATH
 
+# run local config elements
+git clone https://github.com/stfc/cloud-image-builders.git
+DIB_LOCAL_ELEMENTS="vm_baseline"
+export DIB_LOCAL_ELEMENTS
+DIB_LOCAL_ELEMENTS_PATH="$(pwd)/cloud-image-builders/amphora-image-builder/elements"
+export DIB_LOCAL_ELEMENTS_PATH
+
 cd octavia/diskimage-create
 pip3 install -r requirements.txt
 ./diskimage-create.sh -t raw -o "/output/amphora-x64-$(date +"%Y-%m-%d")-haproxy.raw"
