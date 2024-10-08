0f03efe27e8e9d9e543e073e683b7e92d40fc2a5
Alexander Dibbo <alexander.dibbo@stfc.ac.uk>
2024-10-08 14:02:25 +0100

    Address David's comments

--- amphora-image-builder/entrypoint.sh
+++ amphora-image-builder/entrypoint.sh
@@ -12,10 +12,8 @@ DIB_REPO_PATH="$(pwd)/octavia"
 export DIB_REPO_PATH
 
 # run local config elements
-git clone https://github.com/apdibbo/cloud-image-builders.git
-#git clone https://github.com/stfc/cloud-image-builders.git
+git clone https://github.com/stfc/cloud-image-builders.git
 cd /tmp/cloud-image-builders
-git checkout origin/amphora-ral
 chmod 755 amphora-image-builder/elements/vm_baseline/*/*
 cd /tmp
 DIB_LOCAL_ELEMENTS="vm_baseline"
