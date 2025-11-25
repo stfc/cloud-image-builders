6841565d4f33da790cbc6f4c13ea11ca8ce493c3
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2025-11-25 12:29:28 +0000

    MAINT: Quotes for shellcheck
    Add quotes to make shellcheck happy

--- os_builders/rename_images.sh
+++ os_builders/rename_images.sh
@@ -3,5 +3,5 @@
 current_image_name=$1
 new_image_id=$2
 timestamp=$(date +%F)
-openstack image set --deactivate --name "warehoused-${current_image_name}-${timestamp}" $current_image_name
-openstack image set --public --name "${current_image_name}" $new_image_id
+openstack image set --deactivate --name "warehoused-${current_image_name}-${timestamp}" "$current_image_name"
+openstack image set --public --name "${current_image_name}" "$new_image_id"
