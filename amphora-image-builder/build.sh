9badf8446a25603d2b88aa254a00c9eef73afbc0
anish-mudaraddi <anish.mudaraddi@stfc.ac.uk>
2026-03-23 10:57:36 +0000

    Amphora image builder fixes
    changelog:
    
    - use apt to install ansible rather than a virtualenv in vm_baseline
    
    - don't need to setup a venv for octavia install
    requirements - just install pip and use python that comes default in the container
    - there were some missing dependencies - so add that to the dockerfile
    - fix for setting locale in vm_baseline
    - refactor the entrypoint and move a few commands to the docker build file so we can speed up builds
    - allow setting custom output name

--- amphora-image-builder/build.sh
+++ amphora-image-builder/build.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# make a temporary cache for image building - easiest way to avoid out-of-space issues
+mkdir -p /tmp/octavia-cache
+
 # Move up a dir so we can include this git repo in the build
 # so any changes to the elements can be tested
 cd .. 
@@ -9,5 +12,7 @@ docker build -f amphora-image-builder/Dockerfile -t amphora-image-builder:local 
 
 # The Amphora builder requires privileged access to the host
 # to mount /proc and /sys
-docker run --privileged -v "$(pwd)/output":/output amphora-image-builder:local
+# -e OUTPUT_NAME=my-custom-image: to customize image name
+docker run --rm --privileged -v "$(pwd)/output:/output" amphora-image-builder:local
+
 cd amphora-image-builder
--- amphora-image-builder/entrypoint.sh
+++ amphora-image-builder/entrypoint.sh
@@ -1,30 +1,23 @@
 #!/bin/bash
-
 set -euxo pipefail
+
+chmod 755 /opt/cloud-image-builders/amphora-image-builder/elements/vm_baseline/*/*
 
 DIB_RELEASE="jammy"  # Ubuntu 22.04
 export DIB_RELEASE
 
-cd /tmp
-python3 -m virtualenv octavia_disk_image_create
-# shellcheck source=/dev/null
-source octavia_disk_image_create/bin/activate
-
-git clone --depth=1 https://opendev.org/openstack/octavia
-DIB_REPO_PATH="$(pwd)/octavia"
-export DIB_REPO_PATH
-
-# run local config elements
-cd /opt/cloud-image-builders
-chmod 755 amphora-image-builder/elements/vm_baseline/*/*
-
-cd /tmp
 DIB_LOCAL_ELEMENTS="vm_baseline"
 export DIB_LOCAL_ELEMENTS
 
 DIB_LOCAL_ELEMENTS_PATH="/opt/cloud-image-builders/amphora-image-builder/elements"
 export DIB_LOCAL_ELEMENTS_PATH
 
-cd octavia/diskimage-create
-pip3 install -r requirements.txt
-./diskimage-create.sh -t qcow2 -m -o "/output/amphora-x64-$(date +"%Y-%m-%d")-haproxy.qcow2"
+DIB_REPO_PATH="/opt/octavia"
+export DIB_REPO_PATH
+
+export DIB_IMAGE_SIZE=10
+export DIB_MIN_TMPFS=0
+export TMPDIR=/tmp
+
+cd /opt/octavia/diskimage-create/
+./diskimage-create.sh -m -f -s $DIB_IMAGE_SIZE -o "/output/${OUTPUT_NAME:-amphora-x64-$(date +%Y-%m-%d)-haproxy}.qcow2"
