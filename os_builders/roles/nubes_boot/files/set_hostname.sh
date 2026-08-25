61d8441b7bd33b9b10e22482f9220b43f155492b
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2026-08-25 11:02:33 +0100

    BUG - os_builders: typo in hostname
    Typo in set_hostname.sh. Dash should be a dot.

--- os_builders/roles/nubes_boot/files/set_hostname.sh
+++ os_builders/roles/nubes_boot/files/set_hostname.sh
@@ -3,6 +3,6 @@
 set -euxo pipefail
 
 ipaddress=$(hostname -I | sed -e "s/ //g" -e "s/\./-/g")
-FQDN="host-${ipaddress}-nubes.stfc.ac.uk"
+FQDN="host-${ipaddress}.nubes.stfc.ac.uk"
 
 hostnamectl set-hostname "${FQDN}";
